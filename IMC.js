df0eb4ac6f35321363bda884486791eee72f864e
AnaRaissaest <145810460+AnaRaissaest@users.noreply.github.com>
2023-11-19 20:26:39 -0300

    Update IMC.js

--- IMC.js
+++ IMC.js
@@ -5,20 +5,16 @@ document.addEventListener("DOMContentLoaded", function () {
         formulario.addEventListener("submit", function (event) {
             event.preventDefault();
 
-            // Obter os valores do formulário
             const idade = parseFloat(document.getElementById("idade").value);
             const altura = parseFloat(document.getElementById("altura").value);
             const peso = parseFloat(document.getElementById("peso").value);
 
-            // Calcular IMC
             const imc = peso / (altura * altura);
-
-            // Calcular preços para Operadora A
+             //SE DEIXAR A LÓGICA DE CALCULO IGUAL AO DO ARQUIVO ELE RODA COM O VALOR EX.0.02
             const planoABasico = 100 + idade * (10 * (imc / 10));
-            const planoAStandard = (150 + idade * 15) * (imc / 10);
-            const planoAPremium = (200 - (imc + 10) + idade * 20) * (imc / 10);
+            const planoAStandard = 150 + idade * 15;
+            const planoAPremium = 200 - imc * 10 + idade * 20;
 
-            // Calcular fator de comorbidade para Operadora B
             let fatorComorbidade;
             if (imc < 18.5) {
                 fatorComorbidade = 10;
@@ -34,21 +30,19 @@ document.addEventListener("DOMContentLoaded", function () {
                 fatorComorbidade = 30;
             }
 
-            // Calcular preços para Operadora B
             const planoBBasico = 100 + fatorComorbidade * (10 * (imc / 10));
-            const planoBStandard = (150 + fatorComorbidade * 15) * (imc / 10);
-            const planoBPremium = (200 - (imc + 10) + fatorComorbidade * 20) * (imc / 10);
+            const planoBStandard = 150 + fatorComorbidade * 15;
+            const planoBPremium = 200 - imc * 10 + fatorComorbidade * 20;
 
-            // Atualizar os resultados na tabela
-            document.getElementById("planoABasico").textContent = planoABasico.toFixed(2);
-            document.getElementById("planoAStandard").textContent = planoAStandard.toFixed(2);
-            document.getElementById("planoAPremium").textContent = planoAPremium.toFixed(2);
+            document.getElementById("planoABasico").textContent = "R$" + planoABasico.toFixed(2);
+            document.getElementById("planoAStandard").textContent = "R$" + planoAStandard.toFixed(2);
+            document.getElementById("planoAPremium").textContent = "R$" + planoAPremium.toFixed(2);
 
-            document.getElementById("planoBBasico").textContent = planoBBasico.toFixed(2);
-            document.getElementById("planoBStandard").textContent = planoBStandard.toFixed(2);
-            document.getElementById("planoBPremium").textContent = planoBPremium.toFixed(2);
+            document.getElementById("planoBBasico").textContent = "R$" + planoBBasico.toFixed(2);
+            document.getElementById("planoBStandard").textContent = "R$" + planoBStandard.toFixed(2);
+            document.getElementById("planoBPremium").textContent = "R$" + planoBPremium.toFixed(2);
 
-            // Exibir a tabela de resultados
+
             document.getElementById("tabelaResultado").style.display = "table";
         });
     } else {
